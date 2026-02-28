7c1339d758065537c79231dcf837239889a80acb
xuejingyong <614161787@qq.com>
2026-02-28 16:43:05 +0800

    feat:skepton for comment and suspense for performance

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,4 @@
-export default function InvoicesPage() {
+export default async function InvoicesPage() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return <p>Invoices Page</p>;
 }
