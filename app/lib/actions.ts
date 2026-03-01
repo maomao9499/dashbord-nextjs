cc4e22ce4b0f0b1b8786f5f8346aa0f2e7194b0b
xuejingyong <614161787@qq.com>
2026-03-01 20:10:41 +0800

    feat:404 found page and error handle for server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,10 +24,16 @@ export async function createInvoice(formData: FormData) {
   const amountInCents=amount *100;
   const date =new Date().toISOString().split('T')[0];
   console.log(typeof amount)
+  try {
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  `;} catch (error) {
+    console.error(error)
+    return {
+      message:`Database Error :Failen to Create Invoice`,
+    }
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -39,16 +45,23 @@ export async function updateInvoice(id: string,formData: FormData) {
     status:formData.get('status'),
   })
   const amountInCents=amount *100;
-  await sql`
-    UPDATE invoices
-    SET customer_id=${customerId}, amount=${amountInCents}, status=${status}
-    WHERE id=${id}
-  `;
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error);
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id:string){
+  throw new Error('Delete Invoice is not implemented yet');
   await sql`
     DELETE FROM invoices
     WHERE id=${id}
