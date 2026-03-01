3a6c00f2dd4877d876a2ecb2bbc35085391f6bc6
xuejingyong <614161787@qq.com>
2026-03-01 18:35:58 +0800

    feat:数据的增删改查 invoices table

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,33 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParms = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  function Debounce(func: Function, wait: number) {
+    let timeout: NodeJS.Timeout;
+    return function (...args: any[]) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func(...args), wait);
+    };
+  }
+
+  const handleSearch = (term: string) => {
+    // Implement search logic here, e.g., update state or call a search function
+    const params = new URLSearchParams(searchParms);
+    params.set("page", "1"); // Reset to first page on new search
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
+  const debouncedSearch = Debounce(handleSearch, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +36,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => debouncedSearch(e.target.value)}
+        defaultValue={searchParms.get("query")?.toString() || ""}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
